cddfb2981665a37fa267f45f58cec306f51951eb
QGIS Ninja <hrko9gis@gmail.com>
2025-08-09 07:51:01 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // GitHub Pagesでのサブディレクトリ公開時はbaseをリポジトリ名に変更
 export default defineConfig({
   plugins: [react()],
-  base: '/wikipedia-map-app/', // ★必要に応じて変更
+  base: '/WikiWalk/', // ★必要に応じて変更
 })
