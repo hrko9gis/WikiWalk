94a54193bee4221d9f73cf9e723e18a057c66459
QGIS Ninja <hrko9gis@gmail.com>
2025-08-09 13:22:07 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// GitHub Pagesでのサブディレクトリ公開時はbaseをリポジトリ名に変更
-export default defineConfig({
-  plugins: [react()],
-  base: '/WikiWalk/', // ★必要に応じて変更
-})
